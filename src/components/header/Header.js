9e2fec8ce9de3b60276f75048365777fcf7e3bd7
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2021-09-04 02:31:16 -0500

    solucion errores de menu movil

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,6 +6,21 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 
 export const Header = () => {
+
+    const desactivarMenuMovil = () => {
+        const component = document.querySelector('#sidebarMenu');
+        component.style.transform = 'translateX(-100%)';
+        const btnMenu = document.querySelector('.header__input-movil');
+        btnMenu.checked = false;
+    };
+
+    const activarMenuMovil = () => {
+        const component = document.querySelector('#sidebarMenu');
+        component.style.transform = 'translateX(0)';
+        const btnMenu = document.querySelector('.header__input-movil');
+        btnMenu.checked = true;
+    };
+
     return (
         <header className="header-area header-responsive-padding header-height-1">
             <div className="header-bottom sticky-bar">
@@ -46,7 +61,7 @@ export const Header = () => {
                                 <div className="header-action-style d-block ">
 
                                     <input type="checkbox" className="openSidebarMenu header__input-movil" id="openSidebarMenu" />
-                                    <label htmlFor="openSidebarMenu" className="sidebarIconToggle">
+                                    <label htmlFor="openSidebarMenu" className="sidebarIconToggle" >
                                         <div className="spinner diagonal part-1"></div>
                                         <div className="spinner horizontal"></div>
                                         <div className="spinner diagonal part-2"></div>
@@ -54,16 +69,16 @@ export const Header = () => {
                                     <div id="sidebarMenu">
                                         <ul className="sidebarMenuInner">
                                             <li>
-                                                <AnchorLink offset='10' href='#beneficios'>Beneficios para tu empresa</AnchorLink>
+                                                <AnchorLink offset='10' href='#beneficios' onClick={desactivarMenuMovil}>Beneficios para tu empresa</AnchorLink>
                                             </li>
                                             <li>
-                                                <AnchorLink offset='10' href='#como-funciona'>¿Cómo funciona?</AnchorLink>
+                                                <AnchorLink offset='10' href='#como-funciona' onClick={desactivarMenuMovil}>¿Cómo funciona?</AnchorLink>
                                             </li>
                                             <li>
-                                                <AnchorLink offset='50' href='#sobre-nosotros'>Sobre nosotros</AnchorLink>
+                                                <AnchorLink offset='50' href='#sobre-nosotros' onClick={desactivarMenuMovil}>Sobre nosotros</AnchorLink>
                                             </li>
                                             <li>
-                                                <AnchorLink offset='10' href='#lo-quiero-para-mi-empresa'>Lo quiero para mi empresa</AnchorLink>
+                                                <AnchorLink offset='10' href='#lo-quiero-para-mi-empresa' onClick={desactivarMenuMovil}>Lo quiero para mi empresa</AnchorLink>
                                             </li>
                                             <li>
                                                 <a className="buttons__btn buttons__btn-primary header__btn">Entrar</a>
