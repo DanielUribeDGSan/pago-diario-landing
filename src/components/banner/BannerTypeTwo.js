4de628426b64ef96015d4ce761c34544840add49
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2021-09-04 01:54:30 -0500

    proyecto termiando

--- src/components/banner/BannerTypeTwo.js
+++ src/components/banner/BannerTypeTwo.js
@@ -8,7 +8,11 @@ import {
     banner_type_two__descripcion_3
 } from '../../constants/fileWithConstants'
 
+import CountUp from 'react-countup';
+import VisibilitySensor from "react-visibility-sensor";
+
 export const BannerTypeTwo = () => {
+    let view = false;
     return (
         <section className="banner-type-two__bg" id="sobre-nosotros">
             <div className="container">
@@ -24,7 +28,15 @@ export const BannerTypeTwo = () => {
                     <div className="col-lg-4 col-md-6 col-12 mt-lg-0 mt-md-4 mt-4">
                         <div className="d-lg-flex align-items-center justify-content-center" data-aos="fade-down" data-aos-duration="1800" data-aos-once="true" data-aos-delay="200">
                             <article>
-                                <h1 className="text-white" dangerouslySetInnerHTML={{ __html: banner_type_two__titulo_2 }}></h1>
+                                <h1 className="text-white">
+                                    <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+                                        {({ isVisible }) => (
+                                            <div style={{ height: 40 }}>
+                                                {isVisible ? <span>+<CountUp end={600} duration={2} /></span> : null}
+                                            </div>
+                                        )}
+                                    </VisibilitySensor>
+                                </h1>
                                 <p className="text-white fw-400 mt-2" dangerouslySetInnerHTML={{ __html: banner_type_two__descripcion_2 }}></p>
                             </article>
                         </div>
@@ -32,7 +44,15 @@ export const BannerTypeTwo = () => {
                     <div className="col-lg-4 col-md-6 col-12 mt-lg-0 mt-md-4 mt-4">
                         <div className="d-lg-flex align-items-center justify-content-center" data-aos="fade-up" data-aos-duration="1800" data-aos-once="true" data-aos-delay="200">
                             <article>
-                                <h1 className="text-white" dangerouslySetInnerHTML={{ __html: banner_type_two__titulo_3 }}></h1>
+                                <h1 className="text-white">
+                                    <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+                                        {({ isVisible }) => (
+                                            <div style={{ height: 40 }}>
+                                                {isVisible ? <span>+<CountUp end={300} duration={2} /></span> : null}
+                                            </div>
+                                        )}
+                                    </VisibilitySensor>
+                                </h1>
                                 <p className="text-white fw-400 mt-2" dangerouslySetInnerHTML={{ __html: banner_type_two__descripcion_3 }}></p>
                             </article>
                         </div>
